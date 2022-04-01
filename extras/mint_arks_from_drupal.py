cbae3c607579ab9d3573be4e06ff82a966dbed5c
Mark Jordan <mjordan@sfu.ca>
2022-04-01 09:11:51 -0700

    Updated mint_arks_from_drupal.py.

--- extras/mint_arks_from_drupal.py
+++ extras/mint_arks_from_drupal.py
@@ -10,8 +10,9 @@ Drupal module (https://github.com/mjordan/larkm_integration). See that
 module's README for more information.
 
 By default, this output is a JSON list containing objects with the keys
-'title', 'uuid', and 'nid'. If you want to add keys for 'who', 'when',
-and 'policy' ARK metadata, values for those keys will be used.
+'title', 'uuid', and 'nid'. If you add data to the View for 'who', 'when',
+and 'policy' ARK metadata, you will need to modify the code below that
+populates those ARK fields.
 """
 
 # You will need to set these variables.
@@ -41,6 +42,12 @@ for record in records:
     data = {"target": target, "what": record['title']}
     if 'uuid' in record and len(record['uuid']) > 0:
         data['identifier'] = record['uuid']
+    # Data for these ARK fields are not included in the Drupal View
+    # by default. If you want to populate them, you will need to modify
+    # the View to include the source Drupal fields, and modify the code
+    # here to map those values to 'who', 'when', and 'policy' ARK fields.
+    # If left unused, larkm will provide default values (see larkm's
+    # README for more information).
     if 'who' in record and len(record['who']) > 0:
         data['who'] = record['who']
     if 'when' in record and len(record['when']) > 0:
@@ -49,6 +56,8 @@ for record in records:
         data['policy'] = record['policy']
 
     r = requests.post(endpoint, json=data, headers=headers)
+    # For POST (create ARK) requests, if the provided UUID is already
+    # in use, larkm returns a response code of 409.
     if r.status_code != 201:
         print(f"Could not mint ARK for node {record['nid']}. Response code is {r.status_code}.")
 
