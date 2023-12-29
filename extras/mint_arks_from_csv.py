827b7027807088283e9b7271477441c7fd73d5c2
Mark Jordan <mjordan@sfu.ca>
2023-12-28 20:28:26 -0800

    Updated mint_arks_from_csv.py.

--- extras/mint_arks_from_csv.py
+++ extras/mint_arks_from_csv.py
@@ -8,20 +8,24 @@ Optional columns are 'who', 'when', 'policy', and 'uuid'. If 'uuid'
 is present, its value will be used as the identifier string in the ARK.
 
 Usage: 1) Make sure the IP address of the machine running this script is
-present in larkm's "trusted_ips" configuration option. 2) Change the three
+present in larkm's "trusted_ips" configuration option. 2) Change the four
 variables below to your own values. 3) Run python mint_arks_from_csv.py.
 """
 
 input_filename = 'input.csv'
 output_filename = 'output.csv'
 larkm_host = 'http://localhost:8000/'
+# Leave shoulder empty if you want to use larkm's default shoulder.
+shoulder = ''
+# Leave api_key empty if larkm is not configured to use API keys.
+api_key = 'myapikey'
 
 # Open input CSV.
 input_csv_reader_file_handle = open(input_filename, 'r', encoding="utf-8", newline='')
 input_csv_reader = csv.DictReader(input_csv_reader_file_handle)
 input_csv_reader_fieldnames = input_csv_reader.fieldnames
-input_csv_reader_fieldnames.append('arks_local_resolver')
-input_csv_reader_fieldnames.append('arks_n2t_resolver')
+input_csv_reader_fieldnames.append('ark_local_resolver')
+input_csv_reader_fieldnames.append('ark_n2t_resolver')
 
 # Write out CSV with columns from input CSV plus ARKs.
 writer_file_handle = open(output_filename, 'w+', newline='')
@@ -31,7 +35,10 @@ writer.writeheader()
 for row in input_csv_reader:
     larkm_host = larkm_host.rstrip('/')
     endpoint = f'{larkm_host}/larkm'
-    headers = {'Content-Type': 'application/json'}
+    if len(api_key) == 0:
+        headers = {'Content-Type': 'application/json'}
+    else:
+        headers = {'Content-Type': 'application/json', 'Authorization': api_key}
     data = {"target": row['target'], "what": row['title']}
     if 'uuid' in row and len(row['uuid']) > 0:
         data['identifier'] = row['uuid']
@@ -41,16 +48,18 @@ for row in input_csv_reader:
         data['when'] = row['when']
     if 'policy' in row:
         data['policy'] = row['policy']
+    if len(shoulder) > 0:
+        data['shoulder'] = shoulder
 
     r = requests.post(endpoint, json=data, headers=headers)
     if r.status_code == 201:
         body = json.loads(r.text)
-        row['arks_local_resolver'] = f'{larkm_host}/{body["ark"]["ark_string"]}'
-        row['arks_n2t_resolver'] = f'https://n2t.net/{body["ark"]["ark_string"]}'
+        row['ark_local_resolver'] = f'{larkm_host}/{body["ark"]["ark_string"]}'
+        row['ark_n2t_resolver'] = f'https://n2t.net/{body["ark"]["ark_string"]}'
     else:
         print(f"Could not mint ARK. Response code is {r.status_code}.")
-        row['arks_local_resolver'] = 'error'
-        row['arks_n2t_resolver'] = 'error'
+        row['ark_local_resolver'] = 'error'
+        row['ark_n2t_resolver'] = 'error'
 
     writer.writerow(row)
 
