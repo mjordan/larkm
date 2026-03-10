2808f24d65abfe64a421e4f5ddb83ff67313935a
Mark Jordan <mjordan@sfu.ca>
2026-03-09 22:39:45 -0700

    Added ability to confirm ARK resolution to the mint_arks_from_csv.py script.

--- extras/mint_arks_from_csv.py
+++ extras/mint_arks_from_csv.py
@@ -1,14 +1,19 @@
 import csv
-import requests
 import json
+import requests
 
 """
 Input CSV must contain a 'target' column and a 'title' column.
 Optional columns are 'who', 'when', 'policy', and 'uuid'. If 'uuid'
 is present, its value will be used as the identifier string in the ARK.
 
+Columns present in the input CSV other than those named above will be
+written to the output CSV. This allows you to include columns such as
+"node_id" so the output CSV can be used to update items in the target
+website.
+
 Usage: 1) Make sure the IP address of the machine running this script is
-present in larkm's "trusted_ips" configuration option. 2) Change the four
+present in larkm's "trusted_ips" configuration option. 2) Change the six
 variables below to your own values. 3) Run python mint_arks_from_csv.py.
 """
 
@@ -22,12 +27,24 @@ naan = "99999"
 # Leave api_key empty if larkm is not configured to use API keys.
 api_key = "myapikey"
 
+# Set to True to confirm that the ARK was created successfully by
+# requesting a redirection to the "target" value in the input CSV.
+# If this is set to True, your output CSV will contain a column with
+# the header "test_resolution" containing the result of the request.
+confirm_arks = True
+
+####################
+# Create the ARKs. #
+####################
+
 # Open input CSV.
 input_csv_reader_file_handle = open(input_filename, "r", encoding="utf-8", newline="")
 input_csv_reader = csv.DictReader(input_csv_reader_file_handle)
 input_csv_reader_fieldnames = input_csv_reader.fieldnames
 input_csv_reader_fieldnames.append("ark_local_resolver")
 input_csv_reader_fieldnames.append("ark_n2t_resolver")
+if confirm_arks is True:
+    input_csv_reader_fieldnames.append("test_resolution")
 
 # Write out CSV with columns from input CSV plus ARKs.
 writer_file_handle = open(output_filename, "w+", newline="")
@@ -41,7 +58,7 @@ for row in input_csv_reader:
         headers = {"Content-Type": "application/json"}
     else:
         headers = {"Content-Type": "application/json", "Authorization": api_key}
-    data = {"target": row["target"], "what": row["title"]}
+    data = {"target": row["target"], "naan": naan, "what": row["title"]}
     if "uuid" in row and len(row["uuid"]) > 0:
         data["identifier"] = row["uuid"]
     if "who" in row and len(row["who"]) > 0:
@@ -52,14 +69,19 @@ for row in input_csv_reader:
         data["policy"] = row["policy"]
     if len(shoulder) > 0:
         data["shoulder"] = shoulder
-    if len(naan) > 0:
-        data["naan"] = naan
 
     r = requests.post(endpoint, json=data, headers=headers)
     if r.status_code == 201:
         body = json.loads(r.text)
         row["ark_local_resolver"] = f'{larkm_host}/{body["ark"]["ark_string"]}'
         row["ark_n2t_resolver"] = f'https://n2t.net/{body["ark"]["ark_string"]}'
+
+        if confirm_arks is True:
+            cr = requests.get(row["ark_local_resolver"], allow_redirects=False)
+            if cr.headers.get('location') == row["target"]:
+                row["test_resolution"] = "confirmed"
+            else:
+                row["test_resolution"] = "ARK not resolving"
     else:
         print(f"Could not mint ARK. Response code is {r.status_code}.")
         row["ark_local_resolver"] = "error"
