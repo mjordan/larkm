cf49929f1fdf5a141d0d8e3c9a6d45f0268ff91c
Mark Jordan <mjordan@sfu.ca>
2026-04-13 09:48:40 -0700

    Resolves #29.

--- extras/mint_arks_from_csv.py
+++ extras/mint_arks_from_csv.py
@@ -1,5 +1,9 @@
+import os
+import sys
 import csv
 import json
+import argparse
+
 import requests
 
 """
@@ -14,51 +18,90 @@ website.
 
 Usage: 1) Make sure the IP address of the machine running this script is
 present in larkm's "trusted_ips" configuration option. 2) Change the six
-variables below to your own values. 3) Run python mint_arks_from_csv.py.
+variables below to your own values. 3) Run python mint_arks_from_csv.py
+with the desired command-line arguments.
 """
 
-input_filename = "input.csv"
-output_filename = "output.csv"
-larkm_host = "http://localhost:8000/"
-# Leave shoulder empty if you want to use larkm's default shoulder.
-shoulder = ""
-# You must provide a NAAN.
-naan = "99999"
-# Leave api_key empty if larkm is not configured to use API keys.
-api_key = "myapikey"
-
-# Set to True to confirm that the ARK was created successfully by
-# requesting a redirection to the "target" value in the input CSV.
-# If this is set to True, your output CSV will contain a column with
-# the header "test_resolution" containing the result of the request.
-confirm_arks = True
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--input_csv",
+    help="Relative or absolute path to your input CSV. Defaults to input.csv.",
+    default="input.csv",
+)
+parser.add_argument(
+    "--output_csv",
+    help="Relative or absolute path to your output CSV. Defaults to output.csv.",
+    default="output.csv",
+)
+parser.add_argument(
+    "--larkm_host",
+    help="Hostname of the server running larkm, includig the leading https://. Trailing slash is optional. Defaults to http://localhost:8000/.",
+    default="http://localhost:8000/",
+)
+parser.add_argument(
+    "--naan",
+    help="The shoulder to use in the ARKs. Required.",
+    required=True,
+)
+parser.add_argument(
+    "--shoulder",
+    help="The NAAN to use in the ARKs. Defaults to larkm's default shoulder.",
+    default="",
+)
+parser.add_argument(
+    "--larkm_api_key",
+    help="An API key registered with larkm.",
+)
+parser.add_argument(
+    "--larkm_api_key_file",
+    help="Path to a file containing An API key registered with larkm. File should only have a single line, containing the key.",
+)
+parser.add_argument(
+    "--confirm_arks",
+    help='Whether or not to to confirm that the ARK was created successfully by requesting a redirection to the "target" value in the input CSV.',
+    action="store_true",
+)
+args = parser.parse_args()
 
 ####################
 # Create the ARKs. #
 ####################
 
+if args.larkm_api_key is None and args.larkm_api_key_file is not None:
+    if os.path.exists(args.larkm_api_key_file) is True:
+        with open(args.larkm_api_key_file) as f:
+            api_key = f.readline().strip()
+    else:
+        sys.exit(f'Error: API key file "{args.larkm_api_key_file}" not found.')
+elif args.larkm_api_key_file is None and args.larkm_api_key is not None:
+    api_key = args.larkm_api_key
+else:
+    sys.exit(
+        "Either the --larkm_api_key or the --larkm_api_key_file arguments is required."
+    )
+
 # Open input CSV.
-input_csv_reader_file_handle = open(input_filename, "r", encoding="utf-8", newline="")
+input_csv_reader_file_handle = open(args.input_csv, "r", encoding="utf-8", newline="")
 input_csv_reader = csv.DictReader(input_csv_reader_file_handle)
 input_csv_reader_fieldnames = input_csv_reader.fieldnames
 input_csv_reader_fieldnames.append("ark_local_resolver")
 input_csv_reader_fieldnames.append("ark_n2t_resolver")
-if confirm_arks is True:
+if args.confirm_arks is True:
     input_csv_reader_fieldnames.append("test_resolution")
 
 # Write out CSV with columns from input CSV plus ARKs.
-writer_file_handle = open(output_filename, "w+", newline="")
+writer_file_handle = open(args.output_csv, "w+", newline="")
 writer = csv.DictWriter(writer_file_handle, fieldnames=input_csv_reader_fieldnames)
 writer.writeheader()
 
 for row in input_csv_reader:
-    larkm_host = larkm_host.rstrip("/")
+    larkm_host = args.larkm_host.rstrip("/")
     endpoint = f"{larkm_host}/larkm"
     if len(api_key) == 0:
         headers = {"Content-Type": "application/json"}
     else:
         headers = {"Content-Type": "application/json", "Authorization": api_key}
-    data = {"target": row["target"], "naan": naan, "what": row["title"]}
+    data = {"target": row["target"], "naan": args.naan, "what": row["title"]}
     if "uuid" in row and len(row["uuid"]) > 0:
         data["identifier"] = row["uuid"]
     if "who" in row and len(row["who"]) > 0:
@@ -67,8 +110,8 @@ for row in input_csv_reader:
         data["when"] = row["when"]
     if "policy" in row:
         data["policy"] = row["policy"]
-    if len(shoulder) > 0:
-        data["shoulder"] = shoulder
+    if len(args.shoulder) > 0:
+        data["shoulder"] = args.shoulder
 
     r = requests.post(endpoint, json=data, headers=headers)
     if r.status_code == 201:
@@ -76,14 +119,16 @@ for row in input_csv_reader:
         row["ark_local_resolver"] = f'{larkm_host}/{body["ark"]["ark_string"]}'
         row["ark_n2t_resolver"] = f'https://n2t.net/{body["ark"]["ark_string"]}'
 
-        if confirm_arks is True:
+        if args.confirm_arks is True:
             cr = requests.get(row["ark_local_resolver"], allow_redirects=False)
             if cr.headers.get("location") == row["target"]:
                 row["test_resolution"] = "confirmed"
             else:
                 row["test_resolution"] = "ARK not resolving"
     else:
-        print(f"Could not mint ARK. Response code is {r.status_code}.")
+        print(
+            f"Could not mint ARK. Response code is {r.status_code}, response body is {r.text}."
+        )
         row["ark_local_resolver"] = "error"
         row["ark_n2t_resolver"] = "error"
 
@@ -91,4 +136,4 @@ for row in input_csv_reader:
 
 writer_file_handle.close()
 
-print(f"Your CSV containing ARKs is at {output_filename}.")
+print(f"Your CSV containing ARKs is at {args.output_csv}.")
